26b0f7edc856bc96e92b4c3b62685a7738af61e6
beLiked <99529522+beLiked@users.noreply.github.com>
2022-06-06 09:11:57 +0300

    Add files via upload

---
+++ precache-manifest.efe9baab61059910d64971a1024ba431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13d8a5ba98d73b1c5139",
+    "url": "/static/css/main.133da3ee.chunk.css"
+  },
+  {
+    "revision": "13d8a5ba98d73b1c5139",
+    "url": "/static/js/main.13d8a5ba.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "c94d25fd4c23f933cd4f",
+    "url": "/static/js/2.c94d25fd.chunk.js"
+  },
+  {
+    "revision": "b29c9e157adc6ecf5862",
+    "url": "/static/js/runtime~main.b29c9e15.js"
+  },
+  {
+    "revision": "b82a90039d8fd7ad0b8275312f128cbf",
+    "url": "/index.html"
+  }
+];
